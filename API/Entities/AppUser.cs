9c3a261a558570e2495582817a24c92c4f2d342c
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-02-08 17:11:20 +0300

    Added : MemberDto and PhotoDto classes, update UsersController and UserRepository

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -20,9 +20,10 @@ public class AppUser
     public string Country { get; set; }
     public List<Photo> Photos { get; set; } = new();
 
-
+    /*
     public int GetAge()
     {
         return DateOfBirth.CalculateAge();
     }
+    */
 }
