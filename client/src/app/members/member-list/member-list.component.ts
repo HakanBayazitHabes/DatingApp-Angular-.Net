415571916f873e2ccdba1c4e96d54031dec0df29
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-02-18 15:16:47 +0300

    Updated : member-card and member-list components

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -20,6 +20,10 @@ export class MemberListComponent implements OnInit {
   pagination: Pagination | undefined;
   userParams: UserParams | undefined;
   user: User | undefined;
+  genderList = [
+    { value: 'male', display: 'Males' },
+    { value: 'female', display: 'Females' },
+  ];
 
   constructor(
     private memberService: MembersService,
@@ -51,6 +55,14 @@ export class MemberListComponent implements OnInit {
       },
     });
   }
+
+  resetFilters() {
+    if (this.user) {
+      this.userParams = new UserParams(this.user);
+      this.loadMembers();
+    }
+  }
+
   pageChanged($event: PageChangedEvent) {
     if (this.userParams && this.userParams?.pageNumber !== $event.page) {
       this.userParams.pageNumber = $event.page;
