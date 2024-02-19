d72d83757e2e5e37e651adec1e67b9d5f92bd7e3
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-02-19 05:51:43 +0300

    Updated : userParams.ts and members.service.ts

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -12,11 +12,17 @@ import { UserParams } from '../_models/userParams';
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
-  paginationResult: PaginatedResult<Member[]> = new PaginatedResult<Member[]>();
+  memberCache = new Map();
 
   constructor(private http: HttpClient) {}
 
   getMembers(userParams: UserParams) {
+    const response = this.memberCache.get(Object.values(userParams).join('-'));
+
+    if (response) {
+      return of(response);
+    }
+
     let params = this.getPaginationHeaders(
       userParams.pageNumber,
       userParams.pageSize
@@ -27,7 +33,12 @@ export class MembersService {
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
 
-    return this.getPaginationResult<Member[]>(this.baseUrl + 'users', params);
+    return this.getPaginationResult<Member[]>(this.baseUrl + 'users', params).pipe(
+      map(response =>{
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
+      })
+    )
   }
 
   private getPaginationResult<T>(url: string, params: HttpParams) {
