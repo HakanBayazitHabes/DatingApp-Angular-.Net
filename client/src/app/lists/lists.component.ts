19c0b6709ac0a9b5564b05e9395caa9114c02829
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-02-22 00:25:41 +0300

    Refactor likes pagination and add pagination component to lists component

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../_models/member';
 import { MembersService } from '../_services/members.service';
+import { Pagination } from '../_models/pagination';
+import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-lists',
@@ -10,6 +12,9 @@ import { MembersService } from '../_services/members.service';
 export class ListsComponent implements OnInit {
   members: Member[] | undefined;
   predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
+  pagination: Pagination | undefined;
 
   constructor(private memberService: MembersService) {}
   ngOnInit(): void {
@@ -17,8 +22,20 @@ export class ListsComponent implements OnInit {
   }
 
   loadLikes() {
-    this.memberService.getLikes(this.predicate).subscribe((response) => {
-      this.members = response;
-    });
+    this.memberService
+      .getLikes(this.predicate, this.pageNumber, this.pageSize)
+      .subscribe({
+        next: (response) => {
+          this.members = response.result;
+          this.pagination = response.pagination;
+        },
+      });
+  }
+
+  pageChanged($event: PageChangedEvent) {
+    if (this.pageNumber !== $event.page) {
+      this.pageNumber = $event.page;
+      this.loadLikes();
+    }
   }
 }
