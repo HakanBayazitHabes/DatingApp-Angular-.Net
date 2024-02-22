462245090ede2aa011f1db8ee2dc8d617e642b99
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-02-22 22:54:31 +0300

    Added : GetMessageThread endpoint and implement GetMessageThread method in MessageRepository

---
+++ client/src/app/_services/messages.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { getPaginationHeaders, getPaginationResult } from './paginationHelper';
+import { Message } from '../_models/message';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessagesService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getMessage(pagenumber: number, pageSize: number, container: string) {
+    let params = getPaginationHeaders(pagenumber, pageSize);
+    params = params.append('Container', container);
+    return getPaginationResult<Message[]>(
+      this.baseUrl + 'messages',
+      params,
+      this.http
+    );
+  }
+}
