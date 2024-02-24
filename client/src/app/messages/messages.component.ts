4e936308e1e91184a4cb92117337e964472d3c89
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-02-24 07:53:11 +0300

    Fixed : loading indicator in messages component

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -14,6 +14,7 @@ export class MessagesComponent implements OnInit {
   container = 'Unread';
   pageNumber = 1;
   pageSize = 5;
+  loading = false;
 
   constructor(private messageService: MessagesService) {}
 
@@ -22,11 +23,13 @@ export class MessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    this.loading = true;
     this.messageService
       .getMessage(this.pageNumber, this.pageSize, this.container)
       .subscribe((response) => {
         this.messages = response.result;
         this.pagination = response.pagination;
+        this.loading = false;
       });
   }
 
