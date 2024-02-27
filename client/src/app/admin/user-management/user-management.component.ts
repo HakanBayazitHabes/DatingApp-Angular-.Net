ba6d706b7eac0b05c4a80c59ca67936e5bdda4e9
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-02-27 11:05:42 +0300

    Add role update functionality to admin service and user management component

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -13,6 +13,7 @@ export class UserManagementComponent implements OnInit {
   users: User[] = [];
   bsModalRef: BsModalRef<RolesModalComponent> =
     new BsModalRef<RolesModalComponent>();
+  availableRoles = ['Admin', 'Moderator', 'Member'];
 
   constructor(
     private adminService: AdminService,
@@ -29,17 +30,31 @@ export class UserManagementComponent implements OnInit {
     });
   }
 
-  openRolesModal() {
-    const initialState: ModalOptions = {
+  openRolesModal(user: User) {
+    const config: ModalOptions = {
+      class: 'modal-dialog-centered',
       initialState: {
-        list: [
-          'Open a modal with component',
-          'Pass your data',
-          'Do something else',
-        ],
-        title: 'Modal with component',
+        username: user.username,
+        availableRoles: this.availableRoles,
+        selectedRoles: [...user.roles],
       },
     };
-    this.bsModalRef = this.modalService.show(RolesModalComponent, initialState);
+    this.bsModalRef = this.modalService.show(RolesModalComponent, config);
+    this.bsModalRef.onHide?.subscribe({
+      next: () => {
+        const selectedRoles = this.bsModalRef.content?.selectedRoles;
+        if (!this.arrayEqual(selectedRoles!, user.roles)) {
+          this.adminService
+            .updateRoles(user.username, selectedRoles!.join(','))
+            .subscribe({
+              next: (roles) => (user.roles = roles),
+            });
+        }
+      },
+    });
+  }
+
+  private arrayEqual(arr1: any[], arr2: any[]) {
+    return JSON.stringify(arr1.sort()) === JSON.stringify(arr2.sort());
   }
 }
