3c710a420aeb53c24730e016eea6d06e343fd68f
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-04-06 07:16:34 +0300

    Updated : AutoMapperProfiles and LogUserActivity

--- API/Helpers/LogUserActivity.cs
+++ API/Helpers/LogUserActivity.cs
@@ -17,7 +17,7 @@ namespace API.Helpers
             var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
 
             var user = await repo.GetUserByIdAsync(userId);
-            user.LastActive = DateTime.Now;
+            user.LastActive = DateTime.UtcNow;
             await repo.SaveAllAsync();
         }
     }
