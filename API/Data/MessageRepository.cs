54beb81080497054abd41522002dfa36d276764f
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-04-08 02:32:25 +0300

    Refactor GetMessageThread method to improve performance

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -79,9 +79,7 @@ public class MessageRepository : IMessageRepository
 
     public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUserName, string recipientUserName)
     {
-        var messages = await _context.Messages
-            .Include(u => u.Sender).ThenInclude(p => p.Photos)
-            .Include(r => r.Recipient).ThenInclude(p => p.Photos)
+        var query = _context.Messages
             .Where(
                 m => m.RecipientUsername == currentUserName && m.RecipientDeleted == false &&
                 m.SenderUsername == recipientUserName ||
@@ -89,9 +87,9 @@ public class MessageRepository : IMessageRepository
                 m.SenderUsername == currentUserName
             )
             .OrderBy(m => m.MessageSent)
-            .ToListAsync();
+            .AsQueryable();
 
-        var unreadMessages = messages.Where(m => m.DateRead == null &&
+        var unreadMessages = query.Where(m => m.DateRead == null &&
             m.RecipientUsername == currentUserName).ToList();
 
         if (unreadMessages.Any())
@@ -102,7 +100,7 @@ public class MessageRepository : IMessageRepository
             }
         }
 
-        return _mapper.Map<IEnumerable<MessageDto>>(messages);
+        return await query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider).ToListAsync();
 
     }
 
