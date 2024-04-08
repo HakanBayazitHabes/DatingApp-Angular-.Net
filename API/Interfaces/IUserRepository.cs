82b0b9cc43ce948521cf3c67f1cc0321ca317698
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-04-08 03:55:01 +0300

    Refactor user gender retrieval logic in UsersController and UserRepository

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -12,4 +12,5 @@ public interface IUserRepository
     Task<AppUser> GetUserByUsernameAsync(string username);
     Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
     Task<MemberDto> GetMemberDto(string username);
+    Task<string> GetUserGender(string username);
 }
